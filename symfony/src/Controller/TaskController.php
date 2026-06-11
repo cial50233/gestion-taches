4460e1fe6fa74c1a6a0728fc4c4df7c89f7fa389
cial50233 <cial50233@gmail.com>
2026-06-11 09:54:42 +0200

    Ajout de la création et de l'affichage des tâches

--- symfony/src/Controller/TaskController.php
+++ symfony/src/Controller/TaskController.php
@@ -2,15 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepository): Response
     {
-        return $this->render('task/index.html.twig');
+        $tasks = $taskRepository->findAll();
+
+        return $this->render('task/index.html.twig', [
+            'tasks' => $tasks,
+        ]);
+    }
+    
+    #[Route('/task/create', name: 'app_task_create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+
+        $task->setName('Apprendre Symfony');
+        $task->setCompleted(false);
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return new Response('Tâche créée !');
     }
 }
