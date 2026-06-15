96d4f15c34639abdc034d5f683444f96cab2ea8a
cial50233 <cial50233@gmail.com>
2026-06-15 16:41:16 +0200

    Ajout de la validation des tâches

--- symfony/src/Entity/Task.php
+++ symfony/src/Entity/Task.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 class Task
@@ -14,6 +15,11 @@ class Task
     private ?int $id = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\NotBlank(message: 'Le nom est obligatoire')]
+    #[Assert\Length(
+        min: 3,
+        minMessage: 'Le nom doit contenir au moins {{ limit }} caractères'
+    )]
     private ?string $name = null;
 
     #[ORM\Column(nullable: true)]
